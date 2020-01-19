586a710e545243784999f70e4e5a2b56ff2c60bf
Liam Hebert <liamhebert@gmail.com>
2020-01-18 19:33:27 -0500

    Test file for tables

---
+++ test.py
@@ -0,0 +1,65 @@
+import dash
+import dash_table
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import pandas as pd
+
+
+#  df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
+#  d = {'col1': [1, 2], 'col2': [3, 4]}
+
+
+def build_table():
+    return dash_table.DataTable(
+        id='table',
+        data=df.to_dict('records'),
+        columns=[{"name": i, "id": i} for i in df.columns],
+        style_cell={'textAlign': 'left'},
+        row_selectable="multi",
+        selected_rows=[],
+        active_cell={'row': 0, 'column': 2, 'row_id': 1, 'column_id': 'col1'},
+        style_data_conditional=[
+            {
+                'if': {
+                    'column_id': 'Generation (GWh)',
+                    'filter_query': '{Generation (GWh)} > 500'
+                },
+                'color': 'red',
+            },
+        ]
+    )
+
+d = {'col1': [1, 2], 'col2': [3, 4]}
+print(d)
+df = pd.DataFrame(data=d)
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    build_table(),
+    html.Div(id='test'),
+    html.Div(id='test2'),
+])
+
+@app.callback(
+    Output(component_id='test', component_property='children'),
+    [Input(component_id='table', component_property='selected_rows')]
+)
+def update_output_div(input_value):
+    return input_value
+
+
+@app.callback(
+    Output(component_id='test2', component_property='children'),
+    [Input(component_id='table', component_property='active_cell')]
+)
+def find_active_cell(active_cell):
+    print(active_cell)
+    return active_cell['row']
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
