15aa3bb2df5b106f2def9a4e58e2414efdd68b63
Aman Riat <amanriat777@gmail.com>
2020-01-19 00:03:11 -0500

    annual spending

--- app.py
+++ app.py
@@ -13,10 +13,15 @@ from scipy.stats import rayleigh
 from db.api import get_wind_data, get_wind_data_by_id
 
 import pandas as pd
+import test
 
 df = pd.read_csv('transfer_payment.csv', usecols=['FSCL_YR', 'MINC', 'DepartmentNumber-Numéro-de-Ministère',
         'RCPNT_CLS_EN_DESC', 'RCPNT_NML_EN_DESC', 'CTY_EN_NM',
        'PROVTER_EN', 'CNTRY_EN_NM', 'TOT_CY_XPND_AMT', 'AGRG_PYMT_AMT'])
+
+
+for i, row in df.iterrows():
+      df.at[i, 'FSCL_YR'] = 2018
 
 department_dict = pickle.load( open( "department.p", "rb" ) )
 ministry_dict = pickle.load( open( "mine.p", "rb" ) )
@@ -33,7 +38,15 @@ server = app.server
 app_color = {"graph_bg": "#082255", "graph_line": "#007ACE"}
 
 app.layout = html.Div(
-    [
+    [dcc.Dropdown(
+        id='my-dropdown',
+        options=[
+            {'label': 'Coke', 'value': 'COKE'},
+            {'label': 'Tesla', 'value': 'TSLA'},
+            {'label': 'Apple', 'value': 'AAPL'}
+        ],
+        value='COKE'
+    ),
         # header
         html.Div(
             [
@@ -92,50 +105,12 @@ app.layout = html.Div(
                                 html.Div(
                                     [
                                         html.H6(
-                                            "WIND SPEED HISTOGRAM",
-                                            className="graph__title",
+                                            "ANNUAL SPENDING", className="graph__title"
                                         )
                                     ]
                                 ),
-                                html.Div(
-                                    [
-                                        dcc.Slider(
-                                            id="bin-slider",
-                                            min=1,
-                                            max=60,
-                                            step=1,
-                                            value=20,
-                                            updatemode="drag",
-                                            marks={
-                                                20: {"label": "20"},
-                                                40: {"label": "40"},
-                                                60: {"label": "60"},
-                                            },
-                                        )
-                                    ],
-                                    className="slider",
-                                ),
-                                html.Div(
-                                    [
-                                        dcc.Checklist(
-                                            id="bin-auto",
-                                            options=[
-                                                {"label": "Auto", "value": "Auto"}
-                                            ],
-                                            value=["Auto"],
-                                            inputClassName="auto__checkbox",
-                                            labelClassName="auto__label",
-                                        ),
-                                        html.P(
-                                            "# of Bins: Auto",
-                                            id="bin-size",
-                                            className="auto__p",
-                                        ),
-                                    ],
-                                    className="auto__container",
-                                ),
                                 dcc.Graph(
-                                    id="wind-histogram",
+                                    id="annual-spending",
                                     figure=dict(
                                         layout=dict(
                                             plot_bgcolor=app_color["graph_bg"],
@@ -296,169 +271,39 @@ def gen_wind_direction(interval):
 
 
 @app.callback(
-    Output("wind-histogram", "figure"),
-    [Input("wind-speed-update", "n_intervals")],
-    [
-        State("wind-speed", "figure"),
-        State("bin-slider", "value"),
-        State("bin-auto", "value"),
-    ],
+    #'test','children'
+    Output("annual-spending", "figure"), [Input("wind-speed-update", "n_intervals")]
 )
-def gen_wind_histogram(interval, wind_speed_figure, slider_value, auto_state):
+#actual graph
+def gen_wind_histogram(selected_check):
     """
     Genererate wind histogram graph.
     :params interval: upadte the graph based on an interval
-    :params wind_speed_figure: current wind speed graph
-    :params slider_value: current slider value
-    :params auto_state: current auto state
     """
+    #will update to list later for year and total amount
+    total = 0
+    for department in range (len(df['DepartmentNumber-Numéro-de-Ministère'])):
+        if (df['DepartmentNumber-Numéro-de-Ministère'][department] == '001'):
+            total += df['AGRG_PYMT_AMT'][department]
 
-    wind_val = []
-
-    try:
-        # Check to see whether wind-speed has been plotted yet
-        if wind_speed_figure is not None:
-            wind_val = wind_speed_figure["data"][0]["y"]
-        if "Auto" in auto_state:
-            bin_val = np.histogram(
-                wind_val,
-                bins=range(int(round(min(wind_val))), int(round(max(wind_val)))),
-            )
-        else:
-            bin_val = np.histogram(wind_val, bins=slider_value)
-    except Exception as error:
-        raise PreventUpdate
-
-    avg_val = float(sum(wind_val)) / len(wind_val)
-    median_val = np.median(wind_val)
-
-    pdf_fitted = rayleigh.pdf(
-        bin_val[1], loc=(avg_val) * 0.55, scale=(bin_val[1][-1] - bin_val[1][0]) / 3
-    )
-
-    y_val = (pdf_fitted * max(bin_val[0]) * 20,)
-    y_val_max = max(y_val[0])
-    bin_val_max = max(bin_val[0])
-
-    trace = dict(
-        type="bar",
-        x=bin_val[1],
-        y=bin_val[0],
-        marker={"color": app_color["graph_line"]},
-        showlegend=False,
-        hoverinfo="x+y",
-    )
-
-    traces_scatter = [
-        {"line_dash": "dash", "line_color": "#2E5266", "name": "Average"},
-        {"line_dash": "dot", "line_color": "#BD9391", "name": "Median"},
-    ]
-
-    scatter_data = [
+    data = [
         dict(
-            type="scatter",
-            x=[bin_val[int(len(bin_val) / 2)]],
-            y=[0],
-            mode="lines",
-            line={"dash": traces["line_dash"], "color": traces["line_color"]},
-            marker={"opacity": 0},
-            visible=True,
-            name=traces["name"],
+            x=df['FSCL_YR'],
+            y=total,
         )
-        for traces in traces_scatter
     ]
-
-    trace3 = dict(
-        type="scatter",
-        mode="lines",
-        line={"color": "#42C4F7"},
-        y=y_val[0],
-        x=bin_val[1][: len(bin_val[1])],
-        name="Rayleigh Fit",
-    )
     layout = dict(
         height=350,
         plot_bgcolor=app_color["graph_bg"],
         paper_bgcolor=app_color["graph_bg"],
         font={"color": "#fff"},
-        xaxis={
-            "title": "Wind Speed (mph)",
-            "showgrid": False,
-            "showline": False,
-            "fixedrange": True,
-        },
-        yaxis={
-            "showgrid": False,
-            "showline": False,
-            "zeroline": False,
-            "title": "Number of Samples",
-            "fixedrange": True,
-        },
-        autosize=True,
-        bargap=0.01,
-        bargroupgap=0,
-        hovermode="closest",
-        legend={
-            "orientation": "h",
-            "yanchor": "bottom",
-            "xanchor": "center",
-            "y": 1,
-            "x": 0.5,
-        },
-        shapes=[
-            {
-                "xref": "x",
-                "yref": "y",
-                "y1": int(max(bin_val_max, y_val_max)) + 0.5,
-                "y0": 0,
-                "x0": avg_val,
-                "x1": avg_val,
-                "type": "line",
-                "line": {"dash": "dash", "color": "#2E5266", "width": 5},
-            },
-            {
-                "xref": "x",
-                "yref": "y",
-                "y1": int(max(bin_val_max, y_val_max)) + 0.5,
-                "y0": 0,
-                "x0": median_val,
-                "x1": median_val,
-                "type": "line",
-                "line": {"dash": "dot", "color": "#BD9391", "width": 5},
-            },
-        ],
+        autosize=False,
+        xaxis={'type': 'log', 'title': 'Year'},
+        yaxis={'title': 'Payment'},
+        margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
     )
-    return dict(data=[trace, scatter_data[0], scatter_data[1], trace3], layout=layout)
 
-
-@app.callback(
-    Output("bin-auto", "value"),
-    [Input("bin-slider", "value")],
-    [State("wind-speed", "figure")],
-)
-def deselect_auto(slider_value, wind_speed_figure):
-    """ Toggle the auto checkbox. """
-
-    # prevent update if graph has no data
-    if not len(wind_speed_figure["data"]):
-        raise PreventUpdate
-
-    if wind_speed_figure is not None and len(wind_speed_figure["data"][0]["y"]) > 5:
-        return [""]
-    return ["Auto"]
-
-
-@app.callback(
-    Output("bin-size", "children"),
-    [Input("bin-auto", "value")],
-    [State("bin-slider", "value")],
-)
-def show_num_bins(autoValue, slider_value):
-    """ Display the number of bins. """
-
-    if "Auto" in autoValue:
-        return "# of Bins: Auto"
-    return "# of Bins: " + str(int(slider_value))
+    return dict(data=data, layout=layout)
 
 
 if __name__ == "__main__":
